3e0ff6fc0b792faef8497762fce9d3a5c8793bef
Simon Jodet <simon@stoic.com>
2019-03-31 10:27:51 +0200

    New build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,5 +59,6 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['less', 'markdown', 'inline', 'watch']);
+    grunt.registerTask('build', ['less', 'markdown', 'inline']);
 
 };
